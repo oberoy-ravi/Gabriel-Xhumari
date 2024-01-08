198f099202ad15a53f1b1179c3bb0fceeb93d1b7
oberoy-ravi <114639806+oberoy-ravi@users.noreply.github.com>
2024-01-08 15:55:40 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -37,7 +37,28 @@ header.classList.toggle('sticky', window.scrollY > 100);
 /*========== remove menu icon navbar when click navbar link (scroll) ==========*/
 menuIcon.classList.remove('bx-x');
 navbar.classList.remove('active');
+
 };
+
+
+/*========== swiper ==========*/
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 50,
+    loop: true,
+    grabCursor: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+});
+
+
+/*========== dark light mode ==========*/
 
 
 /*========== scroll reveal ==========*/
